8022d50e2c766da469a6351e6bc254f3c64b9366
Austin Kellar <akellar@ucsc.edu>
2018-10-14 13:45:27 -0700

    new exercise

---
+++ web2pyFormsFinished/models/tables.py
@@ -0,0 +1,28 @@
+# Define your tables below (or better in another model file) for example
+#
+# >>> db.define_table('mytable', Field('myfield', 'string'))
+#
+# Fields can be 'string','text','password','integer','double','boolean'
+#       'date','time','datetime','blob','upload', 'reference TABLENAME'
+# There is an implicit 'id integer autoincrement' field
+# Consult manual for more options, validators, etc.
+
+# logger.info("The user record is: %r" % auth.user)
+
+import datetime
+
+
+def get_current_time():
+    return datetime.datetime.utcnow()
+
+
+db.define_table(
+    'movies',
+    Field('title'),
+    Field('description', 'text'),
+    Field('image', 'upload'),
+    Field('date_posted', 'datetime', update=get_current_time())
+)
+
+# after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
